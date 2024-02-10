c5fb7bf1f727dd52388bbf89230b4e8095096c87
nun-ya <68722330+fistonli@users.noreply.github.com>
2024-02-10 12:09:17 -0500

    Update twtichapi.js

--- twtichapi.js
+++ twtichapi.js
@@ -3,6 +3,7 @@ const apiUrl = 'https://api.twitch.tv/helix/';
 const steams = document.getElementById("steamers");
 const bg = document.getElementById("contanter");
 const cinput = document.getElementById("int");
+const cImage = document.getElementById("Image");
 
 let currntstream;//trash lets
 let descript;
@@ -41,28 +42,69 @@ function get(){
   getChannelInfo(array[Math.floor(Math.random() * array.length)])
 }
 function guess(){
-  if(cinput.value.toLowerCase() == starmername.toLowerCase()){
-    console.log('correct');
-    cinput.value = '';
-  }else if(cinput == ""){
+  if(cinput.value.toLowerCase().replace(/[^\w\s]|_/gi, '') == starmername.toLowerCase()){
+    alert("correct!!!")
+    location.reload();
+  }else if(cinput.value == ""){
     alert("Please Insert at least 1 charater")
   }else
   {
-    console.log('incorrect');
+    alert("Inncorrect or misspelled")
     cinput.value = '';
   }
 }
+
+function convertToBlackAndWhiteFromUrl(imageUrl) {
+  return new Promise(function(resolve, reject) {
+    var img = new Image();
+    img.crossOrigin = "Anonymous"; // Allow cross-origin images
+    img.onload = function() {
+      var canvas = document.createElement('canvas');
+      var ctx = canvas.getContext('2d');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      
+      var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      var data = imageData.data;
+      
+      for (var i = 0; i < data.length; i += 4) {
+        var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        data[i] = avg;       // Red
+        data[i + 1] = avg;   // Green
+        data[i + 2] = avg;   // Blue
+      }
+      
+      ctx.putImageData(imageData, 0, 0);
+      
+      var blackAndWhiteImageUrl = canvas.toDataURL();
+      resolve(blackAndWhiteImageUrl);
+    };
+    img.onerror = function(error) {
+      reject(error);
+    };
+    img.src = imageUrl;
+  });
+}
+
 function hint(){
   if(hintused){
     alert("hint has already been used!");
   }else{
-    console.log(currntstream.data[0])
+    convertToBlackAndWhiteFromUrl(currntstream.data[0].profile_image_url)
+    .then(function(blackAndWhiteImageUrl) {
+      cImage.src = blackAndWhiteImageUrl;
+    })
+    .catch(function(error) {
+      alert("an error has apeared!");
+    });
   }
   hintused=true;
 }
 
 
 function build(){
+  console.log(currntstream)
   starmername = currntstream.data[0].display_name.toLowerCase().replace(/[^\w\s]|_/gi, ''); //removes specal charaters
   console.log(starmername);
   regex = new RegExp("\\b" + starmername + "\\b", "i"); 
